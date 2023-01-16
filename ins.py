15142c2030f3e06665bbd0d6af64afb3a1014381
aimesavoir <53520947+aimesavoir@users.noreply.github.com>
2023-01-16 21:21:02 +0100

    Add files via upload

--- ins.py
+++ ins.py
@@ -6,16 +6,27 @@ import glob
 
 def telecharger(dp):
     ig = instaloader.Instaloader()
-    #dp = input("Enter Insta username : ")
+    resulta=st.text('جاري البحث...')
+    
+   
     try:
         ig.download_profile(dp ,profile_pic_only=True)
+        resulta.text('    ')
     except instaloader.ProfileNotExistsException:
         st.warning('إسم مستخدم أنستغرام غير موجود')
+        #resulta.text('إسم مستخدم أنستغرام غير موجود')
 
 #prof= st.text_input('taper le compte instagram')
 #telecharger(prof)
 
-
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 form=st.form('profile')
@@ -28,7 +39,8 @@ if ok==True:
 
 
 for filename in glob.glob(prof+'/*.jpg'): #assuming gif
-    form.image(filename)
+    form.image(filename,width=600)
+    #st.image(filename,ImageFormatOrAuto='auto')
     
 
 
